8de5d5ec96d6ad36850613a5be3acf684298ca8d
Joshua King <windosbk+github@gmail.com>
2018-12-19 18:06:48 +1300

    durr

--- HelloWorld/entrypoint.ps1
+++ HelloWorld/entrypoint.ps1
@@ -1,5 +1,7 @@
-Write-Host "The commit sha was: $GITHUB_SHA
+Write-Host "The commit sha was: $GITHUB_SHA"
 
 if ($args) {
-  Write-Host "Your first argument was: $($args[0])"
+    Write-Host "Your first argument was: $($args[0])"
+} else {
+    Write-Host "You supplied no arguments"
 }
